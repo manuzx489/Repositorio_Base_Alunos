26f678d4103d571b8cdee9e39f512ace5f102e99
manuzx489 <soaresangelomanuele@gmail.com>
2025-09-11 08:44:36 -0300

    Add files via upload

---
+++ Modulo 1/Extra/projeto_de_vendas/pages/02_lanches.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+
+
+st.sidebar.title(" Lanches")
---
+++ Modulo 1/Extra/projeto_de_vendas/roupas.py
@@ -0,0 +1,33 @@
+import streamlit as st
+
+
+
+tab1, tab2, tab3, tab4 = st.tabs(["Kit de Frio", "Calças", "Blusas", "Damais..."])
+
+with tab1 :
+    st.markdown("# Kit de Frio")
+
+
+
+
+
+
+
+with tab2:
+    st.markdown("# Calças")
+
+
+
+
+
+with tab3:
+    st.markdown("# Blusas")
+
+
+
+
+
+with tab4:
+    st.markdown("# Demais...")
+
+
